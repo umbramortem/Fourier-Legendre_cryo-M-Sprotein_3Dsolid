20cad49838a5eedb20a7a85ae3fa1024ac1dbd2d
JAArriagaHernandez <85722819+JAArriagaHernandez@users.noreply.github.com>
2024-05-24 00:39:09 -0600

    SpikesResults.m

--- SpikesResults.m
+++ SpikesResults.m
@@ -40,7 +40,7 @@ clear all; close all;
 %    and perform a segmentation with a simple rectangular region.
 
 %    Reading the video in the storage directory in the PC
-Video = VideoReader('C:\Your PC\Spikes\Videos\Video 1.mov');
+Video = VideoReader('C:\Your PC\Spikes\Videos\Visualization 1.mov');
 
 %    Previous instruction interpreting the video as an structure where 
 %    the frames sizes and number of frames are obtained.
